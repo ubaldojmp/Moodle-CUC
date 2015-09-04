f9c4536343ff2782d5212e75713cc191ec8916d7
ubaldojmp <ubaldojmp@hotmail.com>
2015-09-04 15:48:38 -0500

    Update main.js

--- plugins/contents/main.js
+++ plugins/contents/main.js
@@ -5,6 +5,14 @@ var templates = [
     "root/externallib/text!root/plugins/contents/mimetypes.json"
 ];
 
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-66797288-1', 'auto');
+  ga('send', 'pageview');
+
 define(templates,function (sectionsTpl, contentsTpl, folderTpl, mimeTypes) {
     var plugin = {
         settings: {
